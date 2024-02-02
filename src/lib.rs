48ae9e040635002d2b2655f36a40a8b695918a35
Daniel Mason <daniel@danielmason.com>
2024-02-02 20:40:12 +0000

    Rough styles, Ash forgive me

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use indoc::indoc;
 pub fn App(cx: Scope) -> Element {
     cx.render(rsx! {
         Title {}
+        Intro {}
         Iriss {}
         Footer {}
     })
@@ -25,18 +26,47 @@ fn Title(cx: Scope) -> Element {
     }
 }
 
+fn Intro(cx: Scope) -> Element {
+    render! {
+        article {
+            id: "intro",
+
+            blockquote {
+                p {
+                    indoc! { r#"
+                        "I often hear that Rust is a hard language to learn... but I don't think it is. I want to show
+                        people that, yes, like any language it has its quirks, but today Rust is as easy if not easier
+                        to pick up than many of its contemporaries"
+                    "# }
+                }
+                cite { span {"Daniel"} " " span {"Mason"}}
+            }
+        }
+    }
+}
+
 fn Iriss(cx: Scope) -> Element {
     render! {
         article {
             id: "iriss",
             h2 {
-                "Idiomatic Rust in Simple Steps"
+                span { "Idiomatic" }" "
+                span { "Rust" }" "
+                span { "in" }" "
+                span { "Simple" }" "
+                span { "Steps" }
             }
             section {
                 p {
                     indoc! { "
-                        Idiomatic Rust in Simple Steps is a step-by-step guide to learning Rust no programming knowledge
-                        required. By focusing on idioms, the series aims to get you productive as quickly as possible.
+                        Idiomatic Rust in Simple Steps (IRISS) is a step-by-step guide to learning Rust no programming
+                        knowledge required. By focusing on idioms, the series aims to get you productive as quickly as possible.
+                    " }
+                }
+                p {
+                    indoc! { "
+                        There are two parts to IRISS, a YouTube Series that's easy to watch, and a book / written guide
+                        that provides more details.
                     " }
                 }
             }
@@ -55,7 +85,13 @@ fn Iriss(cx: Scope) -> Element {
 fn Footer(cx: Scope) -> Element {
     render! {
         footer {
-            "GitHub"
+            a {
+                href: "https://github.com/Rust-Simply",
+                img {
+                    src: "./github-mark.svg",
+                    alt: "GitHub"
+                }
+            }
         }
     }
 }
